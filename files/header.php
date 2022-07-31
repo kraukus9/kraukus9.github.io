6ee42b4397a3edb91b01c4b080156c25a25cb2c2
kraukus9 <109706496+kraukus9@users.noreply.github.com>
2022-07-31 14:54:42 -0400

    Update header.php

--- files/header.php
+++ files/header.php
@@ -11,9 +11,6 @@
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="https://reelioz.herokuapp.com/">HOME</a></li>
-			<li class><a href="http://thestreamkrew.ml/">THESTREAMKREW.ML</a></li>
-		    	<li class><a href="https://reelioz.herokuapp.com/strm?channel=world-fishing-network">WORLD FISHING NETWORK</a></li>
-			
                         <!-- Break -->
 						<!-- Break -->
                     </ul>
