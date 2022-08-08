c8a8b52769ade352522fb21db96a69f7ba8d70f1
kraukus9 <109706496+kraukus9@users.noreply.github.com>
2022-08-07 20:43:13 -0400

    Update and rename alt2.php to google-prox.php

---
+++ google-prox.php
@@ -0,0 +1,36 @@
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- This script attemmpts to disable devtools by bringing up the console and sometimes disabling media that is playing -->
+<script src="https://cdn.jsdelivr.net/npm/console-ban@4.1.0/dist/console-ban.min.js"></script>
+<script>
+    // default options
+    ConsoleBan.init()
+    // custom options
+    ConsoleBan.init({
+      redirect: '/'
+    })
+  </script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/clappr/latest/clappr.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/clappr.level-selector/latest/level-selector.min.js"></script>
+<script src="https://cdn.jsdelivr.net/clappr.chromecast-plugin/latest/clappr-chromecast-plugin.js" type="text/javascript"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js"></script>
+</script>
+</head>
+<body style="margin:0px;padding:0px;">
+<div id="player">
+<script type="text/javascript" src="//cdn.jsdelivr.net/gh/kate395/deliver/espnm3u8v2.js"></script>
+<script>
+
+ (function(open){XMLHttpRequest.prototype.open=function(method,url,async,user,pass){
+                 
+
+     if(url.indexOf('aserv-') != -1 )
+                 {rewrittenUrl = "https://images9-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&url="+encodeURIComponent(request.url);}
+
+                 else
+                 {rewrittenUrl=url; }
+
+                 </script>
+</div>
+</body>
