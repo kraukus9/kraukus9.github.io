b4e1cc4b6a0a5737c8221bd26ce253f05e6edbfb
kraukus9 <109706496+kraukus9@users.noreply.github.com>
2022-11-11 19:51:24 -0500

    Update reelz.php

--- strm/reelz.php
+++ strm/reelz.php
@@ -30,7 +30,7 @@ $headers = array();
 $headers[] = 'Authority: api-services.freecast.com';
 $headers[] = 'Accept: application/json, text/plain, */*';
 $headers[] = 'Accept-Language: en-US,en;q=0.9';
-$headers[] = 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY3NzA0NDcwLCJqdGkiOiIyMTExYzk4MDExZjQ0NGRmYmFiNThjYzI0ZWFlMDY3ZiIsInVzZXJfaWQiOjQ1NDc4NX0.eNvBNNzKqSHr5oIR01azD93x1jWLZXobH-dhD0VW1HU';
+$headers[] = 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjY4MjIxMzQ2LCJqdGkiOiI0YThmZmI4YzFkZjg0ZmNmOGQ0OTM2MDBmNDEwMDc2NiIsInVzZXJfaWQiOjQ1NTMzMX0.fXFOwu0oLk8p9AS7mEQjmXxDiOY0PBmKm18qVOeY7xQ';
 $headers[] = 'Origin: https://watch.freecast.com';
 $headers[] = 'Referer: https://watch.freecast.com/';
 $headers[] = 'Sec-Ch-Ua: ^^.Not/A)Brand^^\";v=^^\"99^^\",';
@@ -39,7 +39,7 @@ $headers[] = 'Sec-Ch-Ua-Platform: ^^Windows^^\"\"';
 $headers[] = 'Sec-Fetch-Dest: empty';
 $headers[] = 'Sec-Fetch-Mode: cors';
 $headers[] = 'Sec-Fetch-Site: same-site';
-$headers[] = 'Sotalcloud-Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im5na3owNk8ybTdiZmVMR0JEc0hnSjM1aTd4Uzhiblc3In0.mSR0JZ9W6izTw50AMV68Q3WQbYc2Vh6C0sDv4MV3Yi8';
+$headers[] = 'Sotalcloud-Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkZKOENSbEdlY1VIUzFQTnp4SHNQM0NwRnlRQXhWc0ExIn0.toO8Lq1R08iq149GA0NFf94chwqT0LhCDe3ZS7gZxKs';
 $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
